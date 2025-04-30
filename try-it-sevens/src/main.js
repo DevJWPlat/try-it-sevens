e2fb51e25152aabb51929cb268c4b0d87e799534
Jonny Whittle <jonny@builtbyplatform.co.uk>
2025-04-30 18:30:27 +0000

    Initial Vue 3 + Volt setup

---
+++ try-it-sevens/src/main.js
@@ -0,0 +1,22 @@
+// main.js
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  unstyled: true,
+})
+
+app.mount('#app')
+
+
+
+
+
---
+++ try-it-sevens/vite.config.js
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// Dynamically get full Gitpod hostname from the workspace URL
+let allowedHosts = []
+const rawUrl = process.env.GITPOD_WORKSPACE_URL
+
+if (rawUrl) {
+  const url = new URL(rawUrl)
+  // Gitpod uses this pattern: 5173-yourname-yourproject.ws-region.gitpod.io
+  const port = 5173
+  const fullHost = `${port}-${url.hostname}`
+  allowedHosts = [fullHost]
+}
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  base: '/try-it-sevens/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    allowedHosts,
+  },
+})
+
+
